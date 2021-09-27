69fcf97d4074ceb9769acb65da416dc544208202
Pradyumna Paranjape <pradyparanjpe@rediffmail.com>
2021-09-27 15:49:43 +0530

    Include theme-switch as SETUPFILE
    +INCLUDE doesn't seem to work

--- src/readtheorg_theme-daynight/js/readtheorg-daynight.js
+++ src/readtheorg_theme-daynight/js/readtheorg-daynight.js
@@ -35,16 +35,16 @@ $( document ).ready(function() {
 
     // Shift nav in mobile when clicking the menu.
     $(document).on('click', "[data-toggle='wy-nav-top']", function() {
-      $("[data-toggle='wy-nav-shift']").toggleClass("shift");
-      $("[data-toggle='rst-versions']").toggleClass("shift");
+        $("[data-toggle='wy-nav-shift']").toggleClass("shift");
+        $("[data-toggle='rst-versions']").toggleClass("shift");
     });
     // Close menu when you click a link.
     $(document).on('click', ".wy-menu-vertical .current ul li a", function() {
-      $("[data-toggle='wy-nav-shift']").removeClass("shift");
-      $("[data-toggle='rst-versions']").toggleClass("shift");
+        $("[data-toggle='wy-nav-shift']").removeClass("shift");
+        $("[data-toggle='rst-versions']").toggleClass("shift");
     });
     $(document).on('click', "[data-toggle='rst-current-version']", function() {
-      $("[data-toggle='rst-versions']").toggleClass("shift-up");
+        $("[data-toggle='rst-versions']").toggleClass("shift-up");
     });
     // Make tables responsive
     $("table.docutils:not(.field-list)").wrap("<div class='wy-table-responsive'></div>");
@@ -52,8 +52,8 @@ $( document ).ready(function() {
 
 $( document ).ready(function() {
     $('#text-table-of-contents ul').first().addClass('nav');
-                                        // ScrollSpy also requires that we use
-                                        // a Bootstrap nav component.
+    // ScrollSpy also requires that we use
+    // a Bootstrap nav component.
     $('body').scrollspy({target: '#text-table-of-contents'});
 
     // DON'T add sticky table headers (Fix issue #69?)
